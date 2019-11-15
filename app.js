40cc74d6e47a68b6e4c5e935759371946dd3e0de
Ryanflima <ryanf363@gmail.com>
2019-11-15 15:33:59 -0500

    adding some changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // packages
 const express = require("express");
 const bodyParser = require("body-parser");
+const db = require("./models");
 
 // starting express app
 const app = express();
@@ -36,7 +37,10 @@ app.delete("/delete/:index", function(req, res) {
   res.json(list);
 });
 
-// server listening for request
-app.listen(3000, function() {
-  console.log("server is liT!!!!!!");
+db.sequelize.sync().then(function() {
+  // server listening for request
+  app.listen(3000, function(err) {
+    if (err) console.log(err);
+    console.log("server is liT!!!!!!");
+  });
 });
